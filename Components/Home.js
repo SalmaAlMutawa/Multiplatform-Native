4949d907947ee17bef7f70aebb2808e1fa0bb594
DarthHamza <hamzamakia@gmail.com>
2018-11-29 11:43:13 +0300

    added some components and started with routing

---
+++ Components/Home.js
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+// import TopNavBar from "./Navigation/TopNavBar";
+import AuthButton from "./AuthButton";
+
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        <div className="container text-center">
+          <p>
+            Welcome to <strong>Website Name</strong>! We really hope you enjoy
+            shopping with us
+          </p>
+
+          <Link to="/list">
+            <button className="btn btn-info btn-lg">Start Shopping</button>
+          </Link>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Home;
